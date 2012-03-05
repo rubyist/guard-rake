5cd29c4a3af47552239c488293d78ae9c19cc861
Barry Allard <barry@barryallard.name>
2012-03-05 11:25:24 -0800

    fix for lib/guard/version.rb conflict with guard proper

---
+++ lib/guard/rake/version.rb
@@ -0,0 +1,7 @@
+module Guard
+  module Rake
+    module RakeVersion
+      VERSION = "0.0.4"
+    end
+  end
+end
--- lib/guard/version.rb
+++
@@ -1,5 +0,0 @@
-module Guard
-  module RakeVersion
-    VERSION = "0.0.4"
-  end
-end
