a220ccb0f09293b92f5600d40da6b31bd736bef2
Eduardo de Santana Medeiros <eduardo.ufpb@gmail.com>
2020-06-09 21:32:36 -0300

    Add files via upload

--- lib/guard/rake/task.rb
+++ lib/guard/rake/task.rb
@@ -20,65 +20,25 @@ module Guard
           system("rake <%= t %>")
         end
       <% end %>
-      <% if @watch_rakefile %>
-        watch(%r{^Rakefile$}) do |m|
-          system("rake guard")
-        end<% end %>
     end
     HEREDOC
 
-    # A Rake task that generates Guardfile from rake tasks.
-    #
-    # Inside your Rakefile:
-    #
-    #   require "guard/rake/task"
-    #
-    #   Guard::Rake::Task.new
-    #
-    # Or, your you want to watch Rakefile as well:
-    #
-    #   Guard::Rake::Task.new do |t|
-    #     t.watch_rakefile = true
-    #   end
-    #
-    # You may define your own ERB template:
-    #
-    #    Guard::Rake::Task.new('guard') do |t|
-    #      t.template=File.read('Guardfile.erb')
-    #    end
-    #
-    # All these code will create a task that can be run with:
-    #
-    #   rake guard
-    #
-    # Examples:
-    #
-    #   rake guard
-    #   rake guard GUARDFILE=Guardfile2.rb      # output to differente file
-    #
-    # @public
-    #
     class Task < ::Rake::TaskLib
+
       # Name of task. Defaults to :guard.
       # @public
       attr_writer :name
 
-      # Path to Guardfile.
-      # Setting the GUARDFILE environment variable overrides this.
-      # @public
-      attr_accessor :guardfile
-
-      # Used inside template to generate watch for Rakefile
-      attr_accessor :watch_rakefile
-
       # The template code, default value is Guard::Rake::GUARDFILE_TEMPLATE
       attr_accessor :template
 
+      attr_accessor :guardfile
+
       # @public
-      def initialize(name = :guard)
-        @guardfile      = ENV['GUARDFILE'] || 'Guardfile'
+      def initialize(name = 'guard', guardfile='Guardfile')
         @name           = name
         @template       = Guard::Rake::GUARDFILE_TEMPLATE
+        @guardfile      = guardfile
 
         yield self if block_given?
 
@@ -87,15 +47,14 @@ module Guard
 
       private
 
-
       def define_task
-        desc "Generates a Guardfile from Rake tasks"
+        desc "Create Guardfile from rake tasks."
         task(@name) do
           all_tasks = {}
           current_task = nil
           `rake -P`.each_line do |line|
             if line.start_with? "rake" then
-              current_task = line.strip
+              current_task = line[5..-1].strip
             else
               #its a dependency
               filename = line.strip
@@ -105,10 +64,9 @@ module Guard
               end
             end
           end
-          File.write(guardfile, ERB.new(@template).result(binding))
+          File.write(@guardfile, ERB.new(@template).result(binding))
         end
       end
-
     end
   end
 end
